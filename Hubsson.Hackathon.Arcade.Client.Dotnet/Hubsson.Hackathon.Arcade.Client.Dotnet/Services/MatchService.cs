1eab995fb0511edf99b25ac75dbb939b57d48a9f
Blaskó Gergő <blasko.gergo.hun@gmail.com>
2023-06-03 15:32:09 +0200

    Update MatchService.cs

--- Hubsson.Hackathon.Arcade.Client.Dotnet/Hubsson.Hackathon.Arcade.Client.Dotnet/Services/MatchService.cs
+++ Hubsson.Hackathon.Arcade.Client.Dotnet/Hubsson.Hackathon.Arcade.Client.Dotnet/Services/MatchService.cs
@@ -2,7 +2,6 @@
 using Hubsson.Hackathon.Arcade.Client.Dotnet.Contracts;
 using Hubsson.Hackathon.Arcade.Client.Dotnet.Domain;
 using ClientGameState = Hubsson.Hackathon.Arcade.Client.Dotnet.Domain.ClientGameState;
-
 namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
 {
     public class MatchService
@@ -10,27 +9,22 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
         private MatchRepository _matchRepository;
         private ArcadeSettings _arcadeSettings;
         private readonly ILogger<MatchService> _logger;
-
         public MatchService(ArcadeSettings settings, ILogger<MatchService> logger)
         {
             _matchRepository = new MatchRepository();
             _arcadeSettings = settings;
             _logger = logger;
         }
-        
         public void Init()
         {
             // On Game Init
             throw new NotImplementedException();
         }
-
         public Hubsson.Hackathon.Arcade.Client.Dotnet.Domain.Action Update(ClientGameState gameState)
         {
             _logger.LogInformation($"{gameState}");
             _logger.LogError($"CurrentDirection: {_matchRepository.lastDirection}");
-
             Coordinate head = gameState.players.First(player => player.playerId == _arcadeSettings.TeamId).coordinates.Last();
-
             Dictionary<Direction, int> freeSpaces = new Dictionary<Direction, int>
             {
                 { Direction.Up, 0 },
@@ -38,7 +32,6 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
                 { Direction.Left, 0 },
                 { Direction.Right, 0 }
             };
-
             if (_matchRepository.lastDirection == null)
             {
                 _matchRepository.lastDirection = (Direction)new Random().Next(1, 5);
@@ -51,49 +44,52 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
                     freeSpaces.Remove(oppositeDirection);
                 }
             }
-
             foreach (var direction in freeSpaces.Keys.ToList())
             {
                 Coordinate newHead = Move(head, direction);
-
                 if (IsValidMove(newHead, gameState))
                 {
+                   // freeSpaces[direction] = CountFreeSpacesInArea(newHead, gameState, 0);
                     freeSpaces[direction] = CountFreeSpacesInArea(newHead, gameState, 1);
+                    straightFreeSpaces[direction] = CountFreeSpacesInLine(newHead, gameState, direction);
                 }
                 else
                 {
                     freeSpaces[direction] = int.MinValue;
                 }
             }
-
-            _matchRepository.lastDirection = freeSpaces.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
-
+           // _matchRepository.lastDirection = freeSpaces.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+            _matchRepository.lastDirection = freeSpaces.Aggregate((l, r) => l.Value > r.Value || (l.Value == r.Value && straightFreeSpaces[l.Key] > straightFreeSpaces[r.Key]) ? l : r).Key;
             return new Domain.Action { direction = _matchRepository.lastDirection.Value, iteration = gameState.iteration };
-
         }
-
-
-
+        private int CountFreeSpacesInLine(Coordinate coordinate, ClientGameState state, Direction direction)
+        {
+            int freeSpaces = 0;
+            Coordinate newCoordinate = Move(coordinate, direction);
+            while (IsValidMove(newCoordinate, state))
+            {
+                freeSpaces++;
+                newCoordinate = Move(newCoordinate, direction);
+            }
+            return freeSpaces;
+        }
         private Coordinate Move(Coordinate coordinate, Direction direction)
         {
             switch (direction)
             {
-                case Direction.Down: return new Coordinate { x = coordinate.x, y = coordinate.y + 1 }; 
+                case Direction.Down: return new Coordinate { x = coordinate.x, y = coordinate.y + 1 };
                 case Direction.Up: return new Coordinate { x = coordinate.x, y = coordinate.y - 1 };
                 case Direction.Left: return new Coordinate { x = coordinate.x - 1, y = coordinate.y };
                 case Direction.Right: return new Coordinate { x = coordinate.x + 1, y = coordinate.y };
                 default: throw new ArgumentException($"Invalid direction: {direction}");
             }
         }
-
-
         private bool IsValidMove(Coordinate coordinate, ClientGameState state)
         {
             if (coordinate.x < 0 || coordinate.x > state.width - 1 || coordinate.y < 0 || coordinate.y > state.height - 1)
             {
                 return false;
             }
-
             foreach (var player in state.players)
             {
                 if (player.coordinates.Any(c => c.x == coordinate.x && c.y == coordinate.y))
@@ -101,15 +97,11 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
                     return false;
                 }
             }
-
             return true;
         }
-
-
         private int CountFreeSpacesInArea(Coordinate coordinate, ClientGameState state, int radius)
         {
             int freeSpaces = 0;
-
             for (int dx = -radius; dx <= radius; dx++)
             {
                 for (int dy = -radius; dy <= radius; dy++)
@@ -121,10 +113,8 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
                     }
                 }
             }
-
             return freeSpaces;
         }
-
         private Direction GetOppositeDirection(Direction direction)
         {
             switch (direction)
@@ -136,7 +126,13 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
                 default: throw new ArgumentException($"Invalid direction: {direction}");
             }
         }
-
+        Dictionary<Direction, int> straightFreeSpaces = new Dictionary<Direction, int>
+    {
+        { Direction.Up, 0 },
+        { Direction.Down, 0 },
+        { Direction.Left, 0 },
+        { Direction.Right, 0 }
+    };
         private class MatchRepository
         {
             public Domain.Direction? lastDirection;
